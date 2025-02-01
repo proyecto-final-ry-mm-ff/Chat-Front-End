7e5e445b29c781e35ea52414129ac10fa04b2a73
Felipe <60165757+felipe-feijoo@users.noreply.github.com>
2025-02-01 10:21:51 -0300

    webpack index html

--- WebChat/webpack.config.js
+++ WebChat/webpack.config.js
@@ -1,29 +1,29 @@
 // webpack.config.js
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const dotenv = require('dotenv').config(); // lee .env y carga las variables
 
 module.exports = {
-  mode: 'development', // o 'production' si vas a compilar para producción
-  entry: './code.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'), // carpeta de salida
-    filename: 'code.js',
-  },
-  devServer: {
-    static: path.join(__dirname, 'dist'), // carpeta a servir
-    port: 3000, // puerto donde correrá el dev server
-    open: true, // abre el navegador automáticamente
-  },
-  plugins: [
-    // DefinePlugin reemplaza "process.env" por los valores
-    new webpack.DefinePlugin({
-      'process.env': JSON.stringify(dotenv.parsed),
-      // Equivalente a:
-      // "process.env": {
-      //   "API_URL": JSON.stringify(process.env.API_URL),
-      //   "MI_VARIABLE": JSON.stringify(process.env.MI_VARIABLE)
-      // }
-    }),
-  ],
+    mode: 'development', // o 'production' si vas a compilar para producción
+    entry: './code.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'), // carpeta de salida
+        filename: 'code.js',
+    },
+    devServer: {
+        static: path.join(__dirname, 'dist'), // carpeta a servir
+        port: 3000, // puerto donde correrá el dev server
+        open: true, // abre el navegador automáticamente
+    },
+    plugins: [
+        // DefinePlugin reemplaza "process.env" por los valores
+        new webpack.DefinePlugin({
+            'process.env': JSON.stringify(dotenv.parsed),
+        }),
+        // Genera el index.html final en dist
+        new HtmlWebpackPlugin({
+            template: './index.html', // ruta a tu index.html original
+        }),
+    ],
 };
