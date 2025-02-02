382b43b60324bbd45d396a71537a9135e5b390b4
Felipe <60165757+felipe-feijoo@users.noreply.github.com>
2025-02-02 17:28:40 -0300

    index.html vacio

--- chat-loader/webpack.config.js
+++ chat-loader/webpack.config.js
@@ -1,30 +1,23 @@
-// webpack.config.js
 const path = require('path');
 const webpack = require('webpack');
-const dotenv = require('dotenv').config(); // lee .env y carga las variables
-
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Agregar este plugin
+const dotenv = require('dotenv').config();
 
 module.exports = {
-  mode: 'development', // o 'production' si vas a compilar para producción
+  mode: 'production', // o 'development' para pruebas
   entry: './loader.js',
   output: {
-    path: path.resolve(__dirname, 'dist'), // carpeta de salida
+    path: path.resolve(__dirname, 'dist'),
     filename: 'loader.js',
   },
-  devServer: {
-    static: path.join(__dirname, 'dist'), // carpeta a servir
-    port: 3000, // puerto donde correrá el dev server
-    open: true, // abre el navegador automáticamente
-  },
   plugins: [
-    // DefinePlugin reemplaza "process.env" por los valores
     new webpack.DefinePlugin({
       'process.env': JSON.stringify(dotenv.parsed),
-      // Equivalente a:
-      // "process.env": {
-      //   "API_URL": JSON.stringify(process.env.API_URL),
-      //   "MI_VARIABLE": JSON.stringify(process.env.MI_VARIABLE)
-      // }
+    }),
+    new HtmlWebpackPlugin({
+      template: 'index.html', // Asegura que index.html sea copiado a dist
+      filename: 'index.html',
+      inject: 'body',
     }),
   ],
 };
